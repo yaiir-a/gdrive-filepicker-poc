4e81a266e310f312f9735d97a1a284b0e6db7c95
YahiaWL <yahiaworklaptop@gmail.com>
2019-08-18 22:44:17 +0100

    adding picker template code

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/oauth')
+def oauth():
+    auth_code = request.args.get('foo')
+    return auth_code
