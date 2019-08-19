383f24859ad71a4ec6ddd9d7eb64e8de38ceca8b
YahiaWL <yahiaworklaptop@gmail.com>
2019-08-19 12:28:49 +0100

    trying to revert

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
-@app.route('/oauth')
-def oauth():
-    auth_code = request.args.get('foo')
-    return auth_code
